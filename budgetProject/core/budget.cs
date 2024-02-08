91a89743f19e8104d2bfc6ca497d0bd95988904c
xpejch08 <pejcharstepan@gmail.com>
2024-02-08 21:29:09 +0100

    initial

---
+++ budgetProject/core/budget.cs
@@ -0,0 +1,9 @@
+namespace core;
+
+public class budget
+{
+    public string Name { get; set; }
+    public decimal Amount { get; set; }
+    public string Category { get; set; }
+    public DateTime DateCreated { get; set; }
+}
